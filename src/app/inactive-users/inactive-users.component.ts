a6b219bedf27656fa472dc7e8124e4ee1abca4dc
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-01-07 20:05:20 +0200

    my solution to services assignment

--- src/app/inactive-users/inactive-users.component.ts
+++ src/app/inactive-users/inactive-users.component.ts
@@ -1,15 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-inactive-users',
   templateUrl: './inactive-users.component.html',
-  styleUrls: ['./inactive-users.component.css']
+  styleUrls: ['./inactive-users.component.css'],
 })
 export class InactiveUsersComponent {
   @Input() users: string[];
-  @Output() userSetToActive = new EventEmitter<number>();
 
-  onSetToActive(id: number) {
-    this.userSetToActive.emit(id);
+  constructor(private usersService: UsersService) {}
+
+  setToActive(id: number) {
+    this.usersService.setToActive(id);
   }
 }

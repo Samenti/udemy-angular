08d56bdec4db51f1bcda962dae2ea20ba8c3ffab
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-01-11 17:19:57 +0200

    added custom observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { Observable, Subscriber, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,17 +7,38 @@ import { interval, Subscription } from 'rxjs';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  private firstObsSubscription: Subscription;
+  // private firstObsSubscription: Subscription;
+  private secondObsSubscription: Subscription;
 
   constructor() {}
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe((count) => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
+    // Observable.create() is deprecated:
+    // const customIntervalObservable = Observable.create(observer => {
+    //   let count = 0;
+    //   setInterval(() => {
+    //     observer.next(count);
+    //     count++;
+    //   }, 1000);
+    // });
+    const customIntervalObservable = new Observable((observer) => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        count++;
+      }, 1000);
+    });
+
+    this.secondObsSubscription = customIntervalObservable.subscribe((data) => {
+      console.log(data);
     });
   }
 
   ngOnDestroy(): void {
-    this.firstObsSubscription.unsubscribe();
+    // this.firstObsSubscription.unsubscribe();
+    this.secondObsSubscription.unsubscribe();
   }
 }

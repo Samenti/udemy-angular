c9b1f759ca674ccc1d36475ab347d0ec4822ff8a
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-01-13 21:08:38 +0200

    my solution to the assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  @ViewChild('form') form: NgForm;
+  formData = {
+    email: '',
+    subscription: '',
+    password: '',
+  };
+  submitted = false;
 
+  onSubmit() {
+    if (this.form.valid && this.form.touched) {
+      this.submitted = true;
+      this.formData.email = this.form.value.email;
+      this.formData.subscription = this.form.value.subscription;
+      this.formData.password = this.form.value.password;
+      this.form.reset({ email: '', subscription: 'advanced', password: '' });
+    }
+  }
 }

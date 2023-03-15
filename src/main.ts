0341f47690c4b15ae13c316f8ed8dddb3acac835
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-03-16 00:12:29 +0200

    routing start

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
-import { AnalyticsService } from './app/shared/analytics.service';
 import { environment } from './environments/environment';
 
 if (environment.production) {

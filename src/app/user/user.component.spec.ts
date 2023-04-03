b2b73a72767992be0ea157786bf9e2b537eed0d6
Marton Nemeskeri <nemeskeri.marton@gmail.com>
2023-04-04 01:12:03 +0300

    testing an async service

--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,7 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UserComponent } from './user.component';
 import { UserService } from './user.service';
+import { DataService } from '../shared/data.service';
 
 describe('UserComponent', () => {
   beforeEach(() => {
@@ -48,4 +49,28 @@ describe('UserComponent', () => {
       app.user.name
     );
   });
+
+  it("shouldn't fetch data successfully if not called asynchronously", () => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails').and.returnValue(
+      Promise.resolve('Data')
+    );
+    fixture.detectChanges();
+    expect(app.data).toBe(undefined);
+  });
+
+  it('should fetch data successfully if called asynchronously', waitForAsync(() => {
+    let fixture = TestBed.createComponent(UserComponent);
+    let app = fixture.debugElement.componentInstance;
+    let dataService = fixture.debugElement.injector.get(DataService);
+    let spy = spyOn(dataService, 'getDetails').and.returnValue(
+      Promise.resolve('Data')
+    );
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(app.data).toBe('Data');
+    });
+  }));
 });
